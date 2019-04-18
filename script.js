50431506588779696059d2a9fdd8995b21b9417b
christian <43443788+ccolon10@users.noreply.github.com>
2019-04-18 09:53:43 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+var names = [];
+var prices = [];
+
+$(".newItem").click(function(){
+   var name = $(".itemName").val();
+   var price =parseInt($(".itemPrice").val());
+
+   names.push(name);
+   prices.push(price);
+   
+   //console.log(names);
+   //console.log(prices);
+   console.log(name);
+   console.log(price);
+});
+
+$(".purchase").click(function(){
+   var priceTotal=0;
+   prices.forEach(function(element){
+       priceTotal += element;
+   });
+   $(".totalPrice").text(priceTotal);
+});
+
+
